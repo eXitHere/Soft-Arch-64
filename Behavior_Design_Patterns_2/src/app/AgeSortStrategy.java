86c5b42c63213e3ec51f2cf2e0c46038cb5410fb
exitguy <kookza01@gmail.com>
2021-11-08 12:44:36 +0700

    Finished ?

--- Behavior_Design_Patterns_2/src/app/AgeSortStrategy.java
+++ Behavior_Design_Patterns_2/src/app/AgeSortStrategy.java
@@ -6,5 +6,8 @@ import java.util.List;
 public class AgeSortStrategy implements SortStrategy {
     // YOU MAY ADD UP TO 5 LINES OF CODE BELOW THIS COMMENT !! A LINE OF CODE MAY
     // CONTAIN UP TO ONE SEMI-COLON !!
-
+    @Override
+    public void customSort(List<Person> people) {
+        people.sort(Comparator.comparingInt(Person::getAge));
+    }
 }
