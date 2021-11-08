77e39953af622f49dffc0e2b8ce4b868223428af
exitguy <kookza01@gmail.com>
2021-11-08 12:36:15 +0700

    Setup beh_2

---
+++ Behavior_Design_Patterns_2/src/app/NameSortStrategy.java
@@ -0,0 +1,10 @@
+package app;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class NameSortStrategy implements SortStrategy {
+  // YOU MAY ADD UP TO 5 LINES OF CODE BELOW THIS COMMENT !! A LINE OF CODE MAY
+  // CONTAIN UP TO ONE SEMI-COLON !!
+
+}
